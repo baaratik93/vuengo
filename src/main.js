da5f8ed61e0efa5f55dcb7d116b9bf29a08c0438
Mahmoud THIAM <dooro.mah@gmail.com>
2023-08-21 21:14:59 +0000

    Varibles d'environnement initialisés

--- src/main.js
+++ src/main.js
@@ -2,5 +2,5 @@ import { createApp } from "vue";
 import { createPinia } from "pinia";
 import App from "./App.vue";
 import axios from "axios";
-axios.defaults.baseURL = "http://127.0.0.1:5200/api";
+axios.defaults.baseURL = process.env.VUE_APP_URL_API;
 createApp(App).use(createPinia()).mount("#app");
